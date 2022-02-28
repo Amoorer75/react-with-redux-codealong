e2386ed74ad5292be270eb1555cdccb6d7c4d114
Amoorer75 <Amoorer127@gmail.com>
2022-02-27 19:14:22 -0500

    first init

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer from './store/reducer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+Terms
+Privacy
